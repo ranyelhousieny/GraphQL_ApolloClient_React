42b0e242e7abd44ad6fc72c58251142a29564034
Rany Elhousieny <elhousie@gmail.com>
2021-01-03 18:53:07 -0800

    Print to browser in App.js

--- fetch-data/src/App.js
+++ fetch-data/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const QUERY = gql `query{
+  country(code:"US"){
+    code
+    name
+  }
+}`
 
 function App() {
   return (
     <div>
       <h1>GraphQL Apollo Client in React</h1>
       <h2>By: Rany ElHousieny</h2>
+      <Query query={ QUERY }>
+        { ( { loading, error, data } ) => {
+          if ( loading ) return ( <h1>Loading ...</h1> );
+          if ( data ) return (
+            <div>
+              <hr/>
+                <h1> Data from GraphQL:</h1>
+                <h1>Country Name: { data.country.name }</h1>
+            </div>
+            
+          )
+        }}
+      </Query>
     </div>
   );
 }
